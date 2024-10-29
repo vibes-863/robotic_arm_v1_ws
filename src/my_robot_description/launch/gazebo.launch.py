51de8629774caa35df1aae82d6cf17888f5c2dc6
vibes-863 <vaibhavdpillai863@outlook.com>
2024-10-30 02:05:00 +0700

    Rename robotic_arm_description pkg to my_robot_description

--- src/my_robot_description/launch/gazebo.launch.py
+++ src/my_robot_description/launch/gazebo.launch.py
@@ -10,7 +10,7 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    share_dir = get_package_share_directory('robotic_arm_description')
+    share_dir = get_package_share_directory('my_robot_description')
 
     xacro_file = os.path.join(share_dir, 'urdf', 'robotic_arm.xacro')
     robot_description_config = xacro.process_file(xacro_file)
--- src/my_robot_description/setup.py
+++ src/my_robot_description/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os
 from glob import glob
 
-package_name = 'robotic_arm_description'
+package_name = 'my_robot_description'
 
 setup(
     name=package_name,
