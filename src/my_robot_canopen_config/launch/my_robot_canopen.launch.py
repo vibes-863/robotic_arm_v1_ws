650aa771ee733fb1d863b3a56b3df4e25d711b8e
Vaibhav Dileep Pillai <vaibhavdpillai863@outlook.com>
2024-11-02 01:40:53 +0800

    Update launch file

--- src/my_robot_canopen_config/launch/my_robot_canopen.launch.py
+++ src/my_robot_canopen_config/launch/my_robot_canopen.launch.py
@@ -1,47 +1,43 @@
 import os
 from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
 import launch
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
-      """Generate launch description with multiple components."""
-      path_file = os.path.dirname(__file__)
+    master_bin_path = os.path.join(
+        get_package_share_directory("my_robot_canopen_config"),
+        "config",
+        "my_robot_bus_config",
+        "master.bin",
+    )
+    if not os.path.exists(master_bin_path):
+        master_bin_path = ""
 
-      ld = launch.LaunchDescription()
+    device_container = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("canopen_core"), "launch"),
+                "/canopen.launch.py",
+            ]
+        ),
+        launch_arguments={
+            "master_config": os.path.join(
+                get_package_share_directory("my_robot_canopen_config"),
+                "config",
+                "my_robot_bus_config",
+                "master.dcf",
+            ),
+            "master_bin": master_bin_path,
+            "bus_config": os.path.join(
+                get_package_share_directory("my_robot_canopen_config"),
+                "config",
+                "my_robot_bus_config",
+                "bus.yml",
+            ),
+            "can_interface_name": "vcan0",
+        }.items(),
+    )
 
-
-      device_container = IncludeLaunchDescription(
-          PythonLaunchDescriptionSource(
-              [
-                  os.path.join(get_package_share_directory("canopen_core"), "launch"),
-                  "/canopen.launch.py",
-              ]
-          ),
-          launch_arguments={
-              "master_config": os.path.join(
-                  get_package_share_directory("my_robot_canopen_config"),
-                  "config",
-                  "bus_config",
-                  "master.dcf",
-              ),
-              "master_bin": os.path.join(
-                  get_package_share_directory("my_robot_canopen_config"),
-                  "config",
-                  "bus_config",
-                  "master.bin",
-              ),
-              "bus_config": os.path.join(
-                  get_package_share_directory("my_robot_canopen_config"),
-                  "config",
-                  "bus_config",
-                  "bus.yml",
-              ),
-              "can_interface_name": 'vcan0',
-          }.items(),
-
-      )
-
-      ld.add_action(device_container)
-
-      return ld
+    return LaunchDescription([device_container])
